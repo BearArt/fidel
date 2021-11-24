29562fd0f0e40ee86e1d45cde1941e1faa06b044
BearArt <89163719+BearArt@users.noreply.github.com>
2021-11-24 14:29:57 -0600

    Update SW2.js

--- SW2.js
+++ SW2.js
@@ -1,10 +1,10 @@
-self.addEventListener('install' , function(e) {
+self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open('lista').then(function(cache){
-            return cache.addAll([
+            return cache.addAll ([
                 './',
                 './registro2SW.js',
-                "./apple-icon-144x144-seochecker-manifest-2024.jpg"
+                "./apple-icon-144x144-seochecker-manifest-2524.jpg"
             ]);
         })
     );
